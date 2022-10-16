6645ff372bc5d4f59333edad292f1efb85dd216f
reaq <113785844+reaq@users.noreply.github.com>
2022-10-16 13:13:36 +0800

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,14 @@
+function rolldie() {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    console.log('Rolled: ${roll} ');
+}
+
+function throwDice() {
+    rolldice();
+    rolldice();
+    rolldice();
+    rolldice();
+    rolldice();
+    rolldice();
+
+}
